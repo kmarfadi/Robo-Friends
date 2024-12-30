3dfac85bc82a366d3c346a60f570f24e5b8a48fd
kmarfadi <kh2kh3@gmail.com>
2024-12-30 07:12:58 +0300

    state change migrate to using React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: '',
-    };
-  }
-
+function App() {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: '',
+  //   };
+  // }
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
   // Fetch user data from API when the component mounts
-  componentDidMount() {
-    fetch('https://jsonplaceholder.cypress.io/users')
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.cypress.io/users')
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+    useEffect(()=>{
+      fetch('https://jsonplaceholder.cypress.io/users')
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
-
-  // Handle search input changes
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
-
-  render() {
-    const { robots, searchfield } = this.state;
-
-    // Filter robots based on search input
+      .then((users) => { setRobots( users ) });
+    }, []);
+    
+    const onSearchChange = (event) => {
+      setSearchfield(event.target.value);
+    }
+    
     const filteredRobots = robots.filter((robot) =>
       robot.name.toLowerCase().includes(searchfield.toLowerCase())
     );
@@ -38,13 +40,13 @@ class App extends Component {
     ) : (
       <div className="tc">
         <h1 className="f1">ROBO Friends</h1>
-        <Searchbox searchChange={this.onSearchChange} />
+        <Searchbox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
       </div>
     );
   }
-}
+
 
 export default App;
